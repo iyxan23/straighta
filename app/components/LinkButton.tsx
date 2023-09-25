6ef31c00823d5b44ead9202388ef8a3e5e399bc7
Iyxan23 <nurihsanalghifari@gmail.com>
2023-09-25 14:16:07 +0700

    chore: add some acessibility props for LinkButton

--- app/components/LinkButton.tsx
+++ app/components/LinkButton.tsx
@@ -1,9 +1,17 @@
 import Link from "next/link";
 
-export default function LinkButton({ text, href }: { text: string; href: string }) {
+export default function LinkButton({
+  text,
+  href,
+}: {
+  text: string;
+  href: string;
+}) {
   return (
     <Link
       href={href}
+      tabIndex={1}
+      role="button"
       className="rounded-lg text-lg shadow-md bg-sky-500 active:bg-sky-600 active:scale-[97%] px-8 py-3 align-middle text-center text-white font-bold hover:bg-sky-400 focus:outline-none focus:ring focus:ring-sky-200 hover:shadow-lg transition-all"
     >
       {text}
