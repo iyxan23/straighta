e330ffd0a9fe393edc8afdaea23931c020226502
Iyxan23 <nurihsanalghifari@gmail.com>
2023-09-27 20:48:04 +0700

    chore: use header-provided username for header in dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,3 +1,5 @@
+import { HEADER_TOKEN_USERNAME } from "@/middlewareHeaders";
+import { headers } from "next/headers";
 import React from "react";
 import Navigation from "../../components/client/Navigation";
 import BottomButtonsBar from "./BottomButtonsBar";
@@ -7,6 +9,8 @@ export default function DashboardLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const username = headers().get(HEADER_TOKEN_USERNAME)!;
+
   return (
     <div className="h-screen max-h-screen w-screen flex flex-col-reverse md:flex-col">
       {children}
@@ -14,8 +18,8 @@ export default function DashboardLayout({
         <div className="flex items-center gap-4 w-full">
           <div className="bg-black rounded-full h-full aspect-square" />
           <div>
-            <strong>Nur Ihsan Al Ghifari</strong>
-            <p className="opacity-50 text-sm">What in the world</p>
+            <strong>{username}</strong>
+            <p className="opacity-50 text-sm">PELAJAR</p>
           </div>
         </div>
         <div className="absolute max-md:bottom-2 md:top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
