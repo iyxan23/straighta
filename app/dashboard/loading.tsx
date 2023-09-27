7d72bab5f0c0582374b3419efefbacef20c511be
Iyxan23 <nurihsanalghifari@gmail.com>
2023-09-27 20:48:12 +0700

    chore: add some loading pages for dashboard pages

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,7 @@
+export default function DashboardLoadingPage() {
+  return (
+    <main className="h-[calc(100vh-72px)] bg-slate-100 flex justify-center items-center text-lg font-boldtracking-wide">
+      <p className="animate-pulse">Loading...</p>
+    </main>
+  );
+}
---
+++ app/dashboard/subject/loading.tsx
@@ -0,0 +1,7 @@
+export default function SubjectLoadingPage() {
+  return (
+    <main className="h-[calc(100vh-72px)] bg-slate-100 flex justify-center items-center text-lg font-bold tracking-wide">
+      <p className="animate-pulse">Loading...</p>
+    </main>
+  );
+}
