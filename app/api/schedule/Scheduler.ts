1f8fb86b176584f79fba87ae04cbb38ab572e2c7
Iyxan23 <nurihsanalghifari@gmail.com>
2023-09-27 10:00:43 +0700

    chore: initialize the scheduler

---
+++ app/api/schedule/Scheduler.ts
@@ -0,0 +1,9 @@
+// The one and only, scheduler
+// This is made as a class so it's easier to pass things around
+// Parameters to be passed:
+//  - 
+export default class Scheduler {
+  constructor() {
+
+  }
+}
