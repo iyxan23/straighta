3c1fe726cefded50be2487bed4eb250ca4984012
Iyxan23 <nurihsanalghifari@gmail.com>
2023-10-01 21:02:02 +0700

    fix: attempt to fix empty string key

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,7 +6,7 @@ import StudyHistory from "./StudyHistory";
 export default function DashboardMainPage() {
   return (
     <DashboardAnimatePresence>
-      <div className="flex flex-col md:flex-row md:h-[calc(100%-70px)] bg-slate-100 overflow-y-scroll md:overflow-y-auto">
+      <div key="main" className="flex flex-col md:flex-row md:h-[calc(100%-70px)] bg-slate-100 overflow-y-scroll md:overflow-y-auto">
         <div className="flex flex-col gap-4 w-full bg-white p-8 md:rounded-r-3xl shadow-lg md:max-h-full">
           <h2 className="font-bold text-2xl text-slate-700">Jadwal</h2>
           <div className="flex flex-col md:flex-row h-auto md:h-full w-full gap-2">
