146b577b66eb86eeb3f341eb99c91fae708376ec
Iyxan23 <nurihsanalghifari@gmail.com>
2023-10-01 20:07:36 +0700

    chore: some mobile optimizations

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,14 +6,14 @@ import StudyHistory from "./StudyHistory";
 export default function DashboardMainPage() {
   return (
     <DashboardAnimatePresence>
-      <div className="overflow-y-scroll flex flex-col md:flex-row md:h-[calc(100%-70px)] bg-slate-100">
+      <div className="flex flex-col md:flex-row md:h-[calc(100%-70px)] bg-slate-100 overflow-y-scroll md:overflow-y-auto">
         <div className="flex flex-col gap-4 w-full bg-white p-8 md:rounded-r-3xl shadow-lg md:max-h-full">
           <h2 className="font-bold text-2xl text-slate-700">Jadwal</h2>
           <div className="flex flex-col md:flex-row h-auto md:h-full w-full gap-2">
             <Schedule />
           </div>
         </div>
-        <div className="md:w-80 lg:w-1/3 p-8 h-full md:overflow-y-scroll max-h-full">
+        <div className="md:w-80 lg:w-1/3 p-8 h-full md:overflow-y-scroll max-h-full mb-24 md:mb-0">
           <h2 className="font-bold text-2xl text-slate-700 mb-4">Riwayat</h2>
           <div className="flex flex-col w-full gap-4">
             <StudyHistory />
--- app/dashboard/subject/SubjectsView.tsx
+++ app/dashboard/subject/SubjectsView.tsx
@@ -1,21 +1,21 @@
-'use client';
+"use client";
 
-import CallbackButton from '@/components/CallbackButton';
-import Modal from '@/components/Modal';
+import CallbackButton from "@/components/CallbackButton";
+import Modal from "@/components/Modal";
 import {
   closeNewSubjectModal,
   finishNewSubjectModal,
   setSubjectMaterials,
   setSubjectName,
-} from '@/redux/features/newSubjectModalSlice';
-import { setQuery } from '@/redux/features/querySlice';
-import { useAppDispatch, useAppSelector } from '@/redux/hooks';
-import { useCreateSubjectMutation } from '@/redux/services/subjectApi';
-import { AnimatePresence, motion } from 'framer-motion';
-import React from 'react';
-import SubjectItems from './SubjectItems';
-import SubjectItemSearch from './SubjectItemSearch';
-import { CustomForm } from '@/components';
+} from "@/redux/features/newSubjectModalSlice";
+import { setQuery } from "@/redux/features/querySlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { useCreateSubjectMutation } from "@/redux/services/subjectApi";
+import { AnimatePresence, motion } from "framer-motion";
+import React from "react";
+import SubjectItems from "./SubjectItems";
+import SubjectItemSearch from "./SubjectItemSearch";
+import { CustomForm } from "@/components";
 
 export default function SubjectsView() {
   const query = useAppSelector((state) => state.query.query);
@@ -32,10 +32,10 @@ export default function SubjectsView() {
     useCreateSubjectMutation();
   return (
     <AnimatePresence>
-      <div className='flex flex-col md:items-center w-full'>
-        <div className='sticky -top-8 flex flex-col items-center p-8 w-full bg-slate-100'>
+      <div className="flex flex-col md:items-center w-full">
+        <div className="sticky -top-8 flex flex-col items-center p-8 w-full bg-slate-100">
           <SubjectItemSearch
-            query={query ?? ''}
+            query={query ?? ""}
             onSearch={(query) => {
               dispatch(setQuery(query));
             }}
@@ -44,7 +44,7 @@ export default function SubjectsView() {
             }}
           />
         </div>
-        <div className='flex flex-col mx-8 md:w-2/3 gap-4 mb-8'>
+        <div className="flex flex-col mx-8 md:w-2/3 gap-4 mb-24 md:mb-8">
           {createSubjectLoading && <>Membuat subjek {newSubjectName}...</>}
           <SubjectItems />
         </div>
@@ -54,24 +54,24 @@ export default function SubjectsView() {
           initial={{ opacity: 0, scale: 1.05 }}
           animate={{ opacity: 1, scale: 1 }}
           exit={{ opacity: 0, scale: 1.05 }}
-          transition={{ ease: 'easeInOut' }}
-          key={'modal'}
-          className='z-20 fixed top-0 bottom-0 w-screen h-screen bg-black bg-opacity-10 backdrop-blur-sm flex justify-center items-center'
+          transition={{ ease: "easeInOut" }}
+          key={"modal"}
+          className="z-20 fixed top-0 bottom-0 w-screen h-screen bg-black bg-opacity-10 backdrop-blur-sm flex justify-center items-center"
         >
           <Modal
             content={() => (
               <CustomForm>
-                <h2 className='text-xl font-bold text-slate-700 mb-2'>
+                <h2 className="text-xl font-bold text-slate-700 mb-2">
                   Buat pembelajaran baru
                 </h2>
-                <p className='mb-1 mt-2'>Nama pembelajaran:</p>
+                <p className="mb-1 mt-2">Nama pembelajaran:</p>
                 <input
                   value={newSubjectName}
                   onChange={(ev) => dispatch(setSubjectName(ev.target.value))}
-                  className='border w-full rounded-lg p-2'
-                  type='text'
+                  className="border w-full rounded-lg p-2"
+                  type="text"
                 />
-                <p className='mb-1 mt-2'>
+                <p className="mb-1 mt-2">
                   Materi-materi pembelajaran tersebut:
                 </p>
                 <input
@@ -79,31 +79,32 @@ export default function SubjectsView() {
                   onChange={(ev) =>
                     dispatch(setSubjectMaterials(ev.target.value))
                   }
-                  className='border w-full rounded-lg p-2'
-                  type='text'
+                  className="border w-full rounded-lg p-2"
+                  type="text"
                 />
-                <p className='text-sm text-slate-400 mt-2'>
-                  Pisahkan beberapa materi pembelajaran menggunakan <br /> tanda koma (,)
+                <p className="text-sm text-slate-400 mt-2">
+                  Pisahkan beberapa materi pembelajaran menggunakan <br /> tanda
+                  koma (,)
                 </p>
-                <p className='text-sm text-slate-400'>
+                <p className="text-sm text-slate-400">
                   Contoh: <strong>android, intents, material design</strong>
                 </p>
-                <div className='flex flex-row gap-2 justify-end mt-4'>
+                <div className="flex flex-row gap-2 justify-end mt-4">
                   <CallbackButton
-                    text='Close'
-                    size='md'
-                    className='bg-red-500 hover:bg-red-400 active:bg-red-700 focus:ring-red-200'
+                    text="Close"
+                    size="md"
+                    className="bg-red-500 hover:bg-red-400 active:bg-red-700 focus:ring-red-200"
                     onClick={() => dispatch(closeNewSubjectModal())}
                   />
                   <CallbackButton
-                    text='OK'
-                    size='md'
+                    text="OK"
+                    size="md"
                     onClick={() => {
                       createSubject({
                         title: newSubjectName,
                         materials:
                           newSubjectMaterials.trim().length !== 0
-                            ? newSubjectMaterials.trim().split(',')
+                            ? newSubjectMaterials.trim().split(",")
                             : undefined,
                       });
                       dispatch(finishNewSubjectModal());
