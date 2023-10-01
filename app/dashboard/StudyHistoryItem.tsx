33e6dbfc03455219bd8158f4b00f6b5f5c886ed0
Iyxan23 <nurihsanalghifari@gmail.com>
2023-10-01 13:45:18 +0700

    fix: display subject name for study history item

--- app/dashboard/StudyHistoryItem.tsx
+++ app/dashboard/StudyHistoryItem.tsx
@@ -1,5 +1,8 @@
+import { CustomCard } from "@/components";
 import { StudySession } from "@/lib/types";
 import { useGetMaterialByIdQuery } from "@/redux/services/materialApi";
+import { useLazyGetSubjectByIdQuery } from "@/redux/services/subjectApi";
+import { useEffect } from "react";
 
 export default function HistoryItem({
   studySession,
@@ -12,13 +15,80 @@ export default function HistoryItem({
     materialId,
   } = studySession;
 
-  const { data, error, isFetching } = useGetMaterialByIdQuery({
+  const { data: materialData, error: materialError } = useGetMaterialByIdQuery({
     id: materialId,
   });
 
+  const [
+    fetchSubject,
+    { data: subjectData, error: subjectError, isFetching: subjectIsFetching },
+  ] = useLazyGetSubjectByIdQuery();
+
+  useEffect(() => {
+    if (materialData && !subjectData && !subjectIsFetching) {
+      fetchSubject({ id: materialData.subjectId });
+    }
+  }, [materialData, subjectData, materialId, subjectIsFetching, fetchSubject]);
+
+  if (materialError) {
+    return (
+      <li key={id} className="flex justify-between items-center gap-2">
+        <CustomCard className="p-4" hoverable>
+          <div>
+            <p className="text-sm text-red-400">
+              Err: {JSON.stringify(materialError)}
+            </p>
+          </div>
+        </CustomCard>
+      </li>
+    );
+  }
+
+  if (subjectError) {
+    return (
+      <li key={id} className="flex justify-between items-center gap-2">
+        <CustomCard className="p-4" hoverable>
+          <div>
+            <p className="text-sm text-red-400">
+              Err: {JSON.stringify(subjectError)}
+            </p>
+          </div>
+        </CustomCard>
+      </li>
+    );
+  }
+
   return (
-    <div className="border border-slate-200 bg-white rounded-md w-full select-none p-4 flex flex-col">
-      {JSON.stringify(data) /* todo: design this */}
-    </div>
+    <li key={id} className="flex justify-between items-center gap-2">
+      <CustomCard className="p-4" hoverable>
+        <div>
+          <h4 className="font-bold text-slate-700">
+            {materialData?.title ?? "Loading..."}
+          </h4>
+          <p className="text-sm text-slate-400">
+            {subjectData?.title ?? "Loading..."}
+          </p>
+        </div>
+        <div>
+          <p className="whitespace-nowrap text-slate-700">
+            {start && end
+              ? `${new Date(start).toLocaleTimeString(undefined, {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                  hour12: false,
+                })}-${
+                  end
+                    ? new Date(end).toLocaleTimeString(undefined, {
+                        hour: "2-digit",
+                        minute: "2-digit",
+                        hour12: false,
+                      })
+                    : "Sekarang"
+                }`
+              : "Loading..."}
+          </p>
+        </div>
+      </CustomCard>
+    </li>
   );
 }
