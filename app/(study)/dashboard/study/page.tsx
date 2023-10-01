5d5aee5abeb2184285f3b29a9ef2740ab1c90b20
Iyxan23 <nurihsanalghifari@gmail.com>
2023-10-01 21:00:50 +0700

    feat: change page color according to current agenda

--- app/(study)/dashboard/study/page.tsx
+++ app/(study)/dashboard/study/page.tsx
@@ -1,8 +1,21 @@
+"use client";
+
+import { useAppSelector } from "@/redux/hooks";
 import StudyView from "./StudyView";
 
 export default function StudyPage() {
+  const currentAgenda = useAppSelector((state) => state.study.currentAgenda);
+
   return (
-    <main className="h-screen w-screen bg-sky-500 flex flex-col justify-around items-center">
+    <main
+      className={`h-screen w-screen ${
+        currentAgenda == "study"
+          ? "bg-sky-500"
+          : currentAgenda == "longBreak"
+          ? "bg-violet-700"
+          : "bg-violet-500"
+      } flex flex-col justify-around items-center transition-colors`}
+    >
       <StudyView />
     </main>
   );
