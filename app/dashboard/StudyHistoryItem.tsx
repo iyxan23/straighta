46c17eda09561d4797bea86be5cc19a1483dcb0f
Iyxan23 <nurihsanalghifari@gmail.com>
2023-10-02 14:31:12 +0700

    feat: impl intersectionobserver for StudyHistoryItem

--- app/dashboard/StudyHistoryItem.tsx
+++ app/dashboard/StudyHistoryItem.tsx
@@ -1,8 +1,11 @@
 import { CustomCard } from "@/components";
 import { StudySession } from "@/lib/types";
-import { useGetMaterialByIdQuery } from "@/redux/services/materialApi";
+import {
+  useGetMaterialByIdQuery,
+  useLazyGetMaterialByIdQuery,
+} from "@/redux/services/materialApi";
 import { useLazyGetSubjectByIdQuery } from "@/redux/services/subjectApi";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import formatDistance from "date-fns/formatDistance";
 import addMilliseconds from "date-fns/addMilliseconds";
 import ArrowRightIcon from "mdi-react/ArrowRightIcon";
@@ -14,6 +17,7 @@ export default function HistoryItem({
 }: {
   studySession: StudySession;
 }) {
+  const elemRef = useRef(null);
   const {
     id,
     timestamp: { start, end },
@@ -22,8 +26,28 @@ export default function HistoryItem({
   } = studySession;
   console.log(studySession);
 
-  const { data: materialData, error: materialError } = useGetMaterialByIdQuery({
-    id: materialId,
+  const [fetchMaterial, { data: materialData, error: materialError }] =
+    useLazyGetMaterialByIdQuery();
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (!entry.isIntersecting) return;
+        if (!materialData) {
+          fetchMaterial({
+            id: materialId,
+          });
+        }
+      },
+      { root: null, rootMargin: "0px", threshold: 0.5 },
+    );
+
+    if (elemRef.current) observer.observe(elemRef.current);
+
+    return () => {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      if (elemRef.current) observer.unobserve(elemRef.current);
+    };
   });
 
   const [
@@ -37,9 +61,13 @@ export default function HistoryItem({
     }
   }, [materialData, subjectData, materialId, subjectIsFetching, fetchSubject]);
 
-  if (materialError) {
-    return (
-      <li key={id} className="flex justify-between items-center gap-2">
+  return (
+    <li
+      ref={elemRef}
+      key={id}
+      className="flex justify-between items-center gap-2"
+    >
+      {materialError ? (
         <CustomCard className="p-4" hoverable>
           <div>
             <p className="text-sm text-red-400">
@@ -47,13 +75,7 @@ export default function HistoryItem({
             </p>
           </div>
         </CustomCard>
-      </li>
-    );
-  }
-
-  if (subjectError) {
-    return (
-      <li key={id} className="flex justify-between items-center gap-2">
+      ) : subjectError ? (
         <CustomCard className="p-4" hoverable>
           <div>
             <p className="text-sm text-red-400">
@@ -61,79 +83,75 @@ export default function HistoryItem({
             </p>
           </div>
         </CustomCard>
-      </li>
-    );
-  }
-
-  return (
-    <li key={id} className="flex justify-between items-center gap-2">
-      <CustomCard className="p-4 relative flex flex-col gap-2" hoverable>
-        <div>
-          <h4 className="font-bold text-slate-700">
-            {materialData?.title ?? "Loading..."}
-          </h4>
-          <p className="text-sm text-slate-400 mb-2">
-            {subjectData?.title ?? "Loading..."}
-          </p>
-          <p className="text-sm text-slate-300 absolute top-4 right-4">
-            {start
-              ? new Date(start).toLocaleDateString(undefined, {
-                  month: "short",
-                  day: "numeric",
-                  year: "numeric",
-                })
-              : ""}
-          </p>
-        </div>
-        <div>
-          <p className="text-lg flex flex-row gap-2 items-center">
-            <span className="text-green-700 font-semibold">{85}</span>
-            <ArrowRightIcon size={16} />
-            <span className="text-amber-600 font-semibold">{74}</span>
-          </p>
-        </div>
-        {agendas && (
-          <div className="flex flex-col gap-2 items-end text-sm absolute bottom-4 right-4">
-            <div className="flex flex-row gap-2">
-              <p className="text-slate-400">
-                {formatDistance(
-                  addMilliseconds(new Date(), agendas.study),
-                  new Date(),
-                )}
-              </p>
-              <SchoolIcon className="fill-sky-700 opacity-75" size={16} />
-            </div>
-            <div className="flex flex-row gap-2">
-              <p className="text-slate-400">
-                {formatDistance(
-                  addMilliseconds(new Date(), agendas.break),
-                  new Date(),
-                )}
-              </p>
-              <SleepIcon className=" fill-green-800 opacity-75" size={16} />
-            </div>
+      ) : (
+        <CustomCard className="p-4 relative flex flex-col gap-2" hoverable>
+          <div>
+            <h4 className="font-bold text-slate-700">
+              {materialData?.title ?? "Loading..."}
+            </h4>
+            <p className="text-sm text-slate-400 mb-2">
+              {subjectData?.title ?? "Loading..."}
+            </p>
+            <p className="text-sm text-slate-300 absolute top-4 right-4">
+              {start
+                ? new Date(start).toLocaleDateString(undefined, {
+                    month: "short",
+                    day: "numeric",
+                    year: "numeric",
+                  })
+                : ""}
+            </p>
           </div>
-        )}
-        <div>
-          <p className="whitespace-nowrap text-slate-700">
-            {start
-              ? `${new Date(start).toLocaleTimeString(undefined, {
-                  hour: "2-digit",
-                  minute: "2-digit",
-                  hour12: false,
-                })} - ${
-                  end
-                    ? new Date(end).toLocaleTimeString(undefined, {
-                        hour: "2-digit",
-                        minute: "2-digit",
-                        hour12: false,
-                      })
-                    : "Sekarang"
-                }`
-              : "Loading..."}
-          </p>
-        </div>
-      </CustomCard>
+          <div>
+            <p className="text-lg flex flex-row gap-2 items-center">
+              <span className="text-green-700 font-semibold">{85}</span>
+              <ArrowRightIcon size={16} />
+              <span className="text-amber-600 font-semibold">{74}</span>
+            </p>
+          </div>
+          {agendas && (
+            <div className="flex flex-col gap-2 items-end text-sm absolute bottom-4 right-4">
+              <div className="flex flex-row gap-2">
+                <p className="text-slate-400">
+                  {formatDistance(
+                    addMilliseconds(new Date(), agendas.study),
+                    new Date(),
+                  )}
+                </p>
+                <SchoolIcon className="fill-sky-700 opacity-75" size={16} />
+              </div>
+              <div className="flex flex-row gap-2">
+                <p className="text-slate-400">
+                  {formatDistance(
+                    addMilliseconds(new Date(), agendas.break),
+                    new Date(),
+                  )}
+                </p>
+                <SleepIcon className=" fill-green-800 opacity-75" size={16} />
+              </div>
+            </div>
+          )}
+          <div>
+            <p className="whitespace-nowrap text-slate-700">
+              {start
+                ? `${new Date(start).toLocaleTimeString(undefined, {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    hour12: false,
+                  })} - ${
+                    end
+                      ? new Date(end).toLocaleTimeString(undefined, {
+                          hour: "2-digit",
+                          minute: "2-digit",
+                          hour12: false,
+                        })
+                      : "Sekarang"
+                  }`
+                : "Loading..."}
+            </p>
+          </div>
+        </CustomCard>
+      )}
     </li>
   );
 }
