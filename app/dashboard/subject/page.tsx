24c6a867be4adfed5079411ff30e4f071904de24
Iyxan23 <nurihsanalghifari@gmail.com>
2023-10-05 19:50:50 +0700

    fix: height of subjects page to be the same as dashboard

--- app/dashboard/subject/page.tsx
+++ app/dashboard/subject/page.tsx
@@ -2,7 +2,7 @@ import SubjectsView from "./SubjectsView";
 
 export default function SubjectPage() {
   return (
-    <div className="h-full p-8 bg-slate-100 overflow-y-scroll flex flex-col items-center">
+    <div className="md:h-[calc(100%-70px)] p-8 bg-slate-100 overflow-y-scroll flex flex-col items-center">
       <h1 className="text-3xl font-bold text-slate-700">Pembelajaran</h1>
       <SubjectsView />
       {/* <div className="flex flex-col items-center w-screen">
