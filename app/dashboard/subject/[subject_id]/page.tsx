9ceaed41a6d4afef08d99a7da103cb8e3f49a6cd
Naufal05R <naufalrabbaniihere@gmail.com>
2023-10-10 20:33:50 +0700

    add grid and layout to detail subject page

--- app/dashboard/subject/[subject_id]/page.tsx
+++ app/dashboard/subject/[subject_id]/page.tsx
@@ -1,3 +1,4 @@
+import { CustomCard } from "@/components";
 import prisma from "@/prisma";
 import { materialsAllAvgScore } from "@/prisma/queries/material";
 import { notFound } from "next/navigation";
@@ -24,14 +25,25 @@ export default async function SelectedSubjectPage({
   //   subject.materials[] -> sebuah array
 
   return (
-    <>
-      Subjek {subject.title}, dibuat oleh {subject.owner_username}. Materi yang
-      dimiliki:{" "}
-      <ul>
-        {subject.materials.map((m) => (
-          <li key={m.id}>Materi: {m.title}</li>
+    <div className="flex flex-col md:h-[calc(100%-70px)] p-8 bg-slate-100 overflow-y-scroll">
+      {/* Subjek {subject.title}, dibuat oleh {subject.owner_username}. Materi yang
+      dimiliki:{" "} */}
+      <h2 className="text-4xl font-bold text-slate-700">{subject.title}</h2>
+      <ul className="mt-8 grid md:grid-cols-2 xl:grid-cols-3 gap-4">
+        {subject.materials.map((material) => (
+          <li key={material.id} className="group">
+            <CustomCard className="p-4">
+              <h4 className="inline-flex text-xl font-semibold text-slate-700 items-center justify-between w-full">
+                {material.title}
+                <div className="ml-auto flex w-fit h-8 items-center">
+                  <hr className="w-2 h-[2px] border-none bg-slate-700 rotate-45 -mr-[1.5px] group-hover:-rotate-45 transition"></hr>
+                  <hr className="w-2 h-[2px] border-none bg-slate-700 -rotate-45 -ml-[1.5px] group-hover:rotate-45 transition"></hr>
+                </div>
+              </h4>
+            </CustomCard>
+          </li>
         ))}
       </ul>
-    </>
+    </div>
   );
 }
