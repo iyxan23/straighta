3eb2b858a9aaa10d963f237f617fa860ccf1232d
Naufal05R <naufalrabbaniihere@gmail.com>
2023-10-10 21:01:59 +0700

    fix card header stucture

--- app/dashboard/subject/[subject_id]/page.tsx
+++ app/dashboard/subject/[subject_id]/page.tsx
@@ -33,13 +33,15 @@ export default async function SubjectDetailPage({
         {subject.materials.map((material) => (
           <li key={material.id} className="group">
             <CustomCard className="p-4">
-              <h4 className="inline-flex text-xl font-semibold text-slate-700 items-center justify-between w-full">
-                {material.title}
+              <hgroup className="inline-flex items-center justify-between w-full">
+                <h4 className="text-xl font-semibold text-slate-700">
+                  {material.title}
+                </h4>
                 <div className="ml-auto flex w-fit h-8 items-center">
                   <hr className="w-2 h-[2px] border-none bg-slate-700 rotate-45 -mr-[1.5px] group-hover:-rotate-45 transition"></hr>
                   <hr className="w-2 h-[2px] border-none bg-slate-700 -rotate-45 -ml-[1.5px] group-hover:rotate-45 transition"></hr>
                 </div>
-              </h4>
+              </hgroup>
             </CustomCard>
           </li>
         ))}
