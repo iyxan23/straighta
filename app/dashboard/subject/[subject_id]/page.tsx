5743c0e4d08dc752f882f33630b8c3b3bbb07340
Iyxan23 <nurihsanalghifari@gmail.com>
2023-10-10 19:52:39 +0700

    feat: basic dynamic subject page

--- app/dashboard/subject/[subject_id]/page.tsx
+++ app/dashboard/subject/[subject_id]/page.tsx
@@ -1,7 +1,37 @@
-export default function SelectedSubjectPage() {
+import prisma from "@/prisma";
+import { materialsAllAvgScore } from "@/prisma/queries/material";
+import { notFound } from "next/navigation";
+
+export default async function SelectedSubjectPage({
+  params: { subject_id },
+}: {
+  params: { subject_id: string };
+}) {
+  const subject = await prisma.subject.findFirst({
+    where: { id: Number(subject_id) },
+    include: { materials: true },
+  });
+  if (!subject) {
+    notFound();
+  }
+
+  // untuk sementara, use it as client component yah
+  // yang bisa digunakan:
+  //   subject.id
+  //   subject.materials
+  //   subject.owner_username
+  //   subject.title
+  //   subject.materials[] -> sebuah array
+
   return (
     <>
-      TEST
+      Subjek {subject.title}, dibuat oleh {subject.owner_username}. Materi yang
+      dimiliki:{" "}
+      <ul>
+        {subject.materials.map((m) => (
+          <li key={m.id}>Materi: {m.title}</li>
+        ))}
+      </ul>
     </>
-  )
-};
+  );
+}
