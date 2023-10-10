f14a458499f21bf3cfdc54187c1758a02d53e6c8
Naufal05R <naufalrabbaniihere@gmail.com>
2023-10-10 16:25:46 +0700

    add style to "muat lebih" in SeubjectItemList

--- app/dashboard/subject/SubjectItems.tsx
+++ app/dashboard/subject/SubjectItems.tsx
@@ -54,6 +54,7 @@ export default function SubjectItems(): JSX.Element {
             onClick={() => {
               dispatch(addOffset(LIMIT));
             }}
+            className='bg-sky-500 text-white rounded-full px-4 py-2 mx-auto'
           >
             Muat lebih
           </button>
