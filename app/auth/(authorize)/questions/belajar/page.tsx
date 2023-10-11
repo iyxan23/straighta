32c6c6e628eb4c39ac72dc5a7ad5bb5a9b3a7cae
Naufal05R <naufalrabbaniihere@gmail.com>
2023-10-11 20:40:01 +0700

    add hoverable to card in BelajarPage

--- app/auth/(authorize)/questions/belajar/page.tsx
+++ app/auth/(authorize)/questions/belajar/page.tsx
@@ -9,7 +9,7 @@ const BelajarPage = () => {
       </h3>
       <div className='flex flex-wrap gap-5 justify-center mt-12'>
         {Array.from({ length: 5 }).map((_, index) => (
-          <CustomCard key={index} className="p-4 w-1/4">
+          <CustomCard key={index} className="p-4 w-1/4" hoverable>
             <h4 className="text-xl">Cukup</h4>
             <p className="mt-4">Lorem ipsum dolor sit amet.</p>
           </CustomCard>
